2affc35261c99c6ac8629b7fdf369f6ddfb3eafc
Shivelle <shivelle@gmx.net>
2017-06-26 15:05:35 +0000

    clean up file structure / remove exercise files

--- IntroToNPM/MyShop/listProducts.js
+++
@@ -1,9 +0,0 @@
-console.log("===================");
-console.log("WELCOME TO MY SHOP!");
-console.log("===================");
-
- var faker = require('faker');
- 
- for(var i = 0; i < 10; i++){
-     console.log(faker.company.companyName() + " - " + faker.company.catchPhrase());
- }
